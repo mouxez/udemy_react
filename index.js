117dec6e4c073e9f2c262c4e3d0045fbfe7f1de6
Maksim <mouxez@gmail.com>
2019-12-10 20:35:04 +0300

    test #1

---
+++ index.js
@@ -0,0 +1,30 @@
+var name = "Max";
+console.log(name);
+name = "Elly";
+console.log(name);
+
+// --------------------------
+
+function printName() {
+    var name = "Max";
+    console.log(name);
+}
+printName();
+
+// ----------------------------
+
+var name = "Max";
+
+if (name == "Max") {
+    var fullName = "Max Bor";
+    console.log(fullName);
+}
+
+// ----------------------------
+
+var name = "Max";
+
+if (name == "Max") {
+    let fullName = "Max Bor";
+    const age = 21;
+}
